d9440f0e04213d9f3dd4d0fdf34b721748e04af8
Deadpo_Ol <https://github.com/devoind>
2022-12-01 01:02:59 +0300

    update project

--- todolist/settings.py
+++ todolist/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
         'HOST': env.str('POSTGRES_HOST', 'localhost'),
-        'PORT': env.str('POSTGRES_PORT'),
+        'PORT': env.int('POSTGRES_PORT'),
     }
 }
 
