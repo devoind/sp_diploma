024923d031d16ae265ca117efeb92f89ffb39408
Deadpo_Ol <https://github.com/devoind>
2022-12-04 18:02:33 +0300

    Add profile

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import get_user_model
-from rest_framework import generics
+from django.contrib.auth import get_user_model, login, logout
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer, ProfileSerializer
 
 USER_MODEL = get_user_model()
 
@@ -10,3 +11,27 @@ class RegistrationView(generics.CreateAPIView):
     model = USER_MODEL
     serializer_class = RegistrationSerializer
 
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+
+        return Response(serializer.data)
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = USER_MODEL.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
