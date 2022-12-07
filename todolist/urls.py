344f469df5b9cb1dcc7e80d87b218da5684a2a8e
Deadpo_Ol <https://github.com/devoind>
2022-12-08 01:42:10 +0300

    Add secrets for VK AUTH to docker-compose-ci and action

--- todolist/urls.py
+++ todolist/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
-    path('api/oauth/', include("social_django.urls", namespace="social")),
+    path('core/', include('core.urls')),
+    path('oauth/', include("social_django.urls", namespace="social")),
 ]
