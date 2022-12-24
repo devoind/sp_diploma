d1ec3244a4fdd97c6f90aa789e3b4eb53e3273f3
Deadpo_Ol <https://github.com/devoind>
2022-12-24 14:36:06 +0300

    Loading2 Bot for BM

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -33,3 +33,4 @@ class Command(BaseCommand):
             for item in res.result:
                 offset = item.update_id + 1
                 self.handle_user(item.message)
+
--- bot/tg/client.py
+++ bot/tg/client.py
@@ -1,29 +1,21 @@
 import requests
-from bot.tg import dc
+
+from bot.tg.dc import GetUpdatesResponse, SendMessageResponse, GET_UPDATES_RESPONSE_SCHEMA, SEND_MESSAGE_RESPONSE_SCHEMA
 
 
 class TgClient:
     def __init__(self, token):
         self.token = token
 
-    def get_url(self, method: str) -> str:
-        """
-        URL для запроса к Telegram боту через токен
-        """
-        return f"https://api.telegram.org/bot{self.token}/{method}"
+    def get_url(self, method: str):
+        return f'https://api.telegram.org/bot{self.token}/{method}'
 
-    def get_updates(self, offset: int = 0, timeout: int = 60) -> dc.GetUpdatesResponse:
-        """
-        Получение Telegram ботом сообщений от пользователя
-        """
-        url = self.get_url("getUpdates")
-        response = requests.get(url, params={"offset": offset, "timeout": timeout})
-        return dc.GET_UPDATES_SCHEMA.load(response.json())
+    def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
+        url = self.get_url('getUpdates')
+        response = requests.get(url=url, params={'offset': offset, 'timeout': timeout})
+        return GET_UPDATES_RESPONSE_SCHEMA.load(response.json())
 
-    def send_message(self, chat_id: int, text: str) -> dc.SendMessageResponse:
-        """
-        Получение пользователем сообщений от Telegram бота
-        """
-        url = self.get_url("sendMessage")
-        response = requests.get(url, params={"chat_id": chat_id, "text": text})
-        return dc.SEND_MESSAGE_RESPONSE_SCHEMA.load(response.json())
+    def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
+        url = self.get_url('sendMessage')
+        response = requests.get(url=url, params={'chat_id': chat_id, 'text': text})
+        return SEND_MESSAGE_RESPONSE_SCHEMA.load(response.json())
