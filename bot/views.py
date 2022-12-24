ae159372cc6b9c7a63a04c7de640998ab988176d
Deadpo_Ol <https://github.com/devoind>
2022-12-24 16:44:02 +0300

    Update3 Bot

--- bot/views.py
+++ bot/views.py
@@ -5,32 +5,29 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from bot.models import TgUser
-
-# from .serializers import TgUserSerializer
 from .tg.client import TgClient
 
 
 class BotVerificationView(generics.UpdateAPIView):
     model = TgUser
     permission_classess = [IsAuthenticated]
-    # serializer_class = TgUserSerializer
-    http_method_names = ["patch"]
+    http_method_names = ['patch']
 
     queryset = TgUser.objects.all()
 
     def patch(self, request, *args, **kwargs):
-        verif_code = self.request.data.get("verification_code")
+        verif_code = self.request.data.get('verification_code')
 
         if not verif_code:
-            raise ValidationError({"Указан неверный код проверки!"})
+            raise ValidationError({'Указан неверный код проверки!'})
 
-        tg_client = TgClient(settings.BOT_TOKEN)
+        tg_client = TgClient(settings.TELEGRAM_BOT_TOKEN)
         try:
             tg_user = TgUser.objects.get(verification_code=verif_code)
         except self.model.DoesNotExist:
-            raise ValidationError({"Не существует пользователя с таким кодом!"})
+            raise ValidationError({'Не существует пользователя с таким кодом!'})
 
         tg_user.user = self.request.user
         tg_user.save()
-        tg_client.send_message(chat_id=tg_user.tg_chat_id, text=f"✅ Аккаунт подтвержден!")
+        tg_client.send_message(chat_id=tg_user.tg_chat_id, text=f'✅ Аккаунт подтвержден!')
         return Response(data=verif_code, status=status.HTTP_201_CREATED)
