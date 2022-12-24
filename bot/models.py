95763d70aaea11c4da8bef33cb2e462803660c36
Deadpo_Ol <https://github.com/devoind>
2022-12-24 14:59:42 +0300

    Update4 Bot for BM

--- bot/models.py
+++ bot/models.py
@@ -1,33 +1,10 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
-from django.utils.crypto import get_random_string
 
 
 class TgUser(models.Model):
-    tg_chat_id = models.BigIntegerField(verbose_name='id чата')
-    tg_user_id = models.BigIntegerField(verbose_name='id пользователя', unique=True)
-    verification_code = models.CharField(verbose_name='Код верификации', max_length=12, unique=True)
-    user = models.ForeignKey(
-        'core.User',
-        verbose_name='Пользователь приложения',
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE,
-    )
-    tg_username = models.CharField(
-        max_length=30,
-        validators=[MinLengthValidator(8)],
-        blank=True,
-        null=True,
-        verbose_name='Пользователь',
-    )
-
-    class Meta:
-        verbose_name = 'Пользователь Tg'
-        verbose_name_plural = 'Пользователи Tg'
-
-    def generate_verification_code(self) -> str:
-        code = get_random_string(12)
-        self.verification_code = code
-        self.save()
-        return code
+    tg_chat_id = models.BigIntegerField()
+    tg_user_id = models.BigIntegerField(unique=True)
+    tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)], null=True)
+    user = models.ForeignKey('core.User', null=True, on_delete=models.CASCADE)
+    verification_code = models.CharField(max_length=32, unique=True)
