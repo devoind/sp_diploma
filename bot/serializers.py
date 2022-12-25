dd64191715b3f9eacfecb6717d355fa1f977c231
Deadpo_Ol <https://github.com/devoind>
2022-12-25 18:47:28 +0300

    New fix 100513

--- bot/serializers.py
+++ bot/serializers.py
@@ -5,14 +5,14 @@ from bot.tg.client import TgClient
 from todolist.settings import TELEGRAM_BOT_TOKEN
 
 
-class TgUserSerializer(serializers.ModelSerializer):
+class BotVerifyCodeUpdateView(serializers.ModelSerializer):
     user_id = serializers.CurrentUserDefault
     tg_id = serializers.IntegerField(source='tg_user_id')
     username = serializers.CharField(source='tg_username')
 
     class Meta:
         model = TgUser
-        fields = '__all__'
+        fields = ['tg_id', 'username', 'verification_code', 'user_id']
 
     def update(self, instance, validated_data):
         instance.user = self.context['request'].user
--- bot/tg/client.py
+++ bot/tg/client.py
@@ -1,6 +1,6 @@
 import requests
 
-from bot.tg import dc
+from bot.tg.dc import GetUpdatesResponse, SendMessageResponse, GET_UPDATES_RESPONSE_SCHEMA, SEND_MESSAGE_RESPONSE_SCHEMA
 
 
 class TgClient:
@@ -13,20 +13,20 @@ class TgClient:
         """
         return f'https://api.telegram.org/bot{self.token}/{method}'
 
-    def get_updates(self, offset: int = 0, timeout: int = 60) -> dc.GetUpdatesResponse:
+    def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
         """
         Получение ботом исходящих сообщений от пользователя
         """
         url = self.get_url('getUpdates')
         response = requests.get(url, params={'offset': offset, 'timeout': timeout})
         print(response.json())
-        return dc.GET_UPDATES_RESPONSE_SCHEMA.load(response.json())
+        return GET_UPDATES_RESPONSE_SCHEMA.load(response.json())
 
-    def send_message(self, chat_id: int, text: str) -> dc.SendMessageResponse:
+    def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
         """
         Получение пользователем сообщений от бота
         """
         url = self.get_url('sendMessage')
         response = requests.get(url, params={'chat_id': chat_id, 'text': text})
         print(response.json())
-        return dc.SEND_MESSAGE_RESPONSE_SCHEMA.load(response.json())
+        return SEND_MESSAGE_RESPONSE_SCHEMA.load(response.json())
