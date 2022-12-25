4ea3f73ad54b2aa722996f670c64f1553f9277f3
Deadpo_Ol <https://github.com/devoind>
2022-12-25 15:37:05 +0300

    New fix 100504

--- core/models.py
+++ core/models.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+# from django.db import models
 
 
 class User(AbstractUser):
-    """ Абстрактный базовый класс, реализующий полнофункциональную пользовательскую модель с правами администратора.
-       Имя пользователя и пароль обязательны. Другие поля являются необязательными. """
-    first_name = models.CharField(max_length=50, verbose_name='Имя')
-    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
-    username = models.CharField(max_length=50, unique=True, verbose_name='Имя пользователя')
-    email = models.EmailField(max_length=100, unique=False, null=True, verbose_name='e-mail')
+    pass
+    # """ Абстрактный базовый класс, реализующий полнофункциональную пользовательскую модель с правами администратора.
+    #    Имя пользователя и пароль обязательны. Другие поля являются необязательными. """
+    # first_name = models.CharField(max_length=50, verbose_name='Имя')
+    # last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    # username = models.CharField(max_length=50, unique=True, verbose_name='Имя пользователя')
+    # email = models.EmailField(max_length=100, unique=False, null=True, verbose_name='e-mail')
 
     class Meta:
         verbose_name = 'Пользователь'
