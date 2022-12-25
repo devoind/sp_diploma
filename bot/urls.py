56f8914065e7570e1553a5f05603a57c02c47f52
Deadpo_Ol <https://github.com/devoind>
2022-12-25 19:08:08 +0300

    New fix 100516

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from bot.views import BotVerifyCodeUpdate
+
+from bot.views import BotVerificationView
 
 urlpatterns = [
-    path('verify', BotVerifyCodeUpdate.as_view(), name='verify'),
+    path("verify", BotVerificationView.as_view(), name="verify"),
 ]
--- core/models.py
+++ core/models.py
@@ -1,20 +1,21 @@
 from django.contrib.auth.models import AbstractUser
-
 from django.db import models
 
 
-class UserRole(models.TextChoices):
-    ADMIN = 'admin'
-    USER = 'user'
-
-
 class User(AbstractUser):
-    phone = models.CharField(max_length=12)
-    role = models.CharField(max_length=5, choices=UserRole.choices, default=UserRole.USER)
+    """ Абстрактный базовый класс, реализующий полнофункциональную пользовательскую модель с правами администратора.
+       Имя пользователя и пароль обязательны. Другие поля являются необязательными. """
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    username = models.CharField(max_length=50, unique=True, verbose_name='Имя пользователя')
+    email = models.EmailField(max_length=100, unique=False, null=True, verbose_name='e-mail')
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
     # def __str__(self):
     #     return f"{self.first_name} {self.last_name}"
