bed76ffa90f6872f0925585c6a01afc2556dc425
Deadpo_Ol <https://github.com/devoind>
2022-12-25 23:16:55 +0300

    Big update3 my project

--- bot/tg/__init__.py
+++ bot/tg/__init__.py
@@ -1,8 +1,9 @@
 import os
 
+from todolist.settings import TELEGRAM_BOT_TOKEN
 from .client import TgClient
 from .dc import get_updates_schema, send_message_schema
 
-tg_client = TgClient(os.environ.get('TELEGRAM_BOT_TOKEN'))
+tg_client = TgClient(TELEGRAM_BOT_TOKEN)
 
 __all__ = ("get_updates_schema", "send_message_schema", "tg_client", "TgClient")
