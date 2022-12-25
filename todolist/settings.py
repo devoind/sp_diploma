57f0daccf72a2984a0f72120293acd643ffddb9d
Deadpo_Ol <https://github.com/devoind>
2022-12-25 17:08:40 +0300

    New fix 100509

--- todolist/settings.py
+++ todolist/settings.py
@@ -107,6 +107,7 @@ DATABASES = {
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 # Password validation
