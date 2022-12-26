c1e897123222503d7c06807ef1acad0d1b3944a1
Deadpo_Ol <https://github.com/devoind>
2022-12-27 00:32:32 +0300

    NewNew fix_project

--- todolist/settings.py
+++ todolist/settings.py
@@ -91,16 +91,15 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST'),
+        'PORT': env.str('POSTGRES_PORT'),
+    }
 }
-#     {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': env.str('POSTGRES_DB'),
-#     'USER': env.str('POSTGRES_USER'),
-#     'PASSWORD': env.str('POSTGRES_PASSWORD'),
-#     'HOST': env.str('POSTGRES_HOST'),
-#     'PORT': env.str('POSTGRES_PORT'),
-# }
 
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
@@ -179,5 +178,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
 
 TELEGRAM_BOT_TOKEN = env.str('TELEGRAM_BOT_TOKEN')
