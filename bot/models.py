7a49c30edc512f429370b5b3e6f10031385b8ab2
Deadpo_Ol <https://github.com/devoind>
2022-12-28 00:58:31 +0300

    This is fix2 new_Bot

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 import string
@@ -9,6 +10,8 @@ class TgUser(models.Model):
     tg_user_id = models.BigIntegerField(unique=True)
     user = models.ForeignKey('core.User', null=True, blank=True, verbose_name='Пользователь', on_delete=models.CASCADE)
     verification_code = models.CharField(max_length=6, unique=True, null=True, blank=True)
+    tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)], null=True, blank=True,
+                                   verbose_name="Имя пользователя")
 
     def set_verification_code(self):
         code = string.digits + string.ascii_letters
