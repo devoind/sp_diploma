f676fe7236f357461c147d1bee5a20509dba12db
Deadpo_Ol <https://github.com/devoind>
2022-12-27 21:19:15 +0300

    New_BOT

--- bot/views.py
+++ bot/views.py
@@ -1,30 +1,35 @@
-from django.conf import settings
+from django.contrib.sites import requests
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from bot.models import TgUser
+from bot.serializers import TgUserSerializer
 from bot.tg.client import TgClient
+from goals.models import BoardParticipant, Board
+from todolist import settings
 
 
-class VerificationCodeView(UpdateAPIView):
-    queryset = TgUser.objects.all()
+# Create your views here.
+class VerificationView(GenericAPIView):
+    model = TgUser
     permission_classes = [IsAuthenticated]
+    serializer_class = TgUserSerializer
 
-    def patch(self, request, *args, **kwargs):
-        ver_code = request.data.get('verification_code')
+    def patch(self, request: requests, *args: str, **kwargs: int) -> Response:
+        serializer: TgUserSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        if not ver_code:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            tg_user = self.get_queryset().get(verification_code=ver_code)
-        except TgUser.DoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
+        tg_user: TgUser = serializer.validated_data['tg_user']
         tg_user.user = self.request.user
-        tg_user.save()
-        TgClient(token=settings.TELEGRAM_BOT_TOKEN).send_message(tg_user.tg_chat_id, 'Вы верифицированы')
-        return Response(status=status.HTTP_200_OK)
+        tg_user.save(update_fields=['user'])
+        instance_serializer: TgUserSerializer = self.get_serializer(tg_user)
+        board = Board(title='Telegram board')
+        board.save()
+        board_p = BoardParticipant(board=board, user=tg_user.user)
+        board_p.save()
+        tg_client = TgClient(settings.TELEGRAM_BOT_TOKEN)
+        tg_client.send_message(tg_user.tg_chat_id, 'Вы успешно подтвердили свою личность.')
+
+        return Response(instance_serializer.data)
