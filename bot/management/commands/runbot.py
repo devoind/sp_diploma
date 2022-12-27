11f17a42cc0411b797f631ea0331b8069e951a9d
Deadpo_Ol <https://github.com/devoind>
2022-12-27 21:35:50 +0300

    New_BOT

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -3,15 +3,13 @@ from django.core.management import BaseCommand
 from bot.models import TgUser
 from bot.tg.client import TgClient
 from bot.tg.dc import Message
-
 from bot.utils_for_bot import BotGoal
 from todolist import settings
 
 
 class Command(BaseCommand):
-    help = 'Run Telegram Bot'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: str, **kwargs: int):
         super().__init__(*args, **kwargs)
         self.tg_client = TgClient(settings.TELEGRAM_BOT_TOKEN)
 
@@ -57,43 +55,3 @@ class Command(BaseCommand):
             for item in res.result:
                 offset = item.update_id + 1
                 self.add_user(item.message)
-
-    # def handle(self, *args, **kwargs):
-    #     offset = 0
-    #
-    #     while True:
-    #         response = self.tg_client.get_updates(offset=offset)
-    #         for item in response.result:
-    #             offset = item.update_id + 1
-    #             tg_user, created = TgUser.objects.get_or_create(tg_chat_id=item.message.chat.id,
-    #                                                             tg_user_id=item.message.from_.id)
-    #
-    #             if created:
-    #                 tg_user.set_verification_code()
-    #                 self.tg_client.send_message(tg_user.tg_chat_id,
-    #                                             f'Привет. Подтверди, пожалуйста, свой аккаунт. '
-    #                                             f'Для подтверждения необходимо ввести код: '
-    #                                             f'{tg_user.verification_code}')
-    #                 continue
-    #
-    #             elif not tg_user.user:
-    #                 tg_user.set_verification_code()
-    #                 self.tg_client.send_message(tg_user.tg_chat_id,
-    #                                             f'Для дальнейшей работы подтвердите, пожалуйста, свой аккаунт. '
-    #                                             f'Для подтверждения необходимо ввести код: '
-    #                                             f'{tg_user.verification_code}')
-    #                 continue
-    #
-    #             if item.message.text == '/goals':
-    #                 user = tg_user.user
-    #                 goals = Goal.objects.filter(category__board__participants__user=user)
-    #
-    #                 for goal in goals:
-    #                     self.tg_client.send_message(tg_user.tg_chat_id,
-    #                                                 f'Название {goal.title}, \n '
-    #                                                 f'Категория {goal.category}, \n'
-    #                                                 f'Статус {goal.get_status_display()}, \n'
-    #                                                 f'Дедлайн {goal.due_date if goal.due_date else "Нет"} \n')
-    #
-    #             else:
-    #                 self.tg_client.send_message(tg_user.tg_chat_id, 'Неизвестная команда')
--- bot/models.py
+++ bot/models.py
@@ -1,20 +1,23 @@
+import random
+import string
+
 from django.db import models
 
-import string
-import random
+from core.models import User
 
 
+# Create your models here.
 class TgUser(models.Model):
     tg_chat_id = models.BigIntegerField(verbose_name='TG CHAT_ID')
     tg_user_id = models.BigIntegerField(unique=True, verbose_name='TG USER_ID')
-    user = models.ForeignKey('core.User', null=True, blank=True, verbose_name='Пользователь',
-                             on_delete=models.PROTECT)
-    verification_code = models.CharField(max_length=6, unique=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, default=None)
+    verification_code = models.CharField(max_length=10, unique=True)
     username = models.CharField(
         max_length=255,
         verbose_name='TG USERNAME',
         null=True,
-        blank=True
+        blank=True,
+        default=None
     )
 
     def set_verification_code(self) -> None:
@@ -24,5 +27,5 @@ class TgUser(models.Model):
         self.verification_code = v_code
 
     class Meta:
-        verbose_name = 'Telegram пользователь'
-        verbose_name_plural = 'Telegram пользователи'
+        verbose_name = 'TG User'
+        verbose_name_plural = 'TG Users'
