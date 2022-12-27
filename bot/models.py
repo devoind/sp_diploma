6edad183dfa9deea4d6955cbcb83eab7e2caefa7
Deadpo_Ol <https://github.com/devoind>
2022-12-27 21:56:19 +0300

    New_BOT

--- bot/models.py
+++ bot/models.py
@@ -1,31 +1,26 @@
-import random
-import string
-
 from django.db import models
 
-from core.models import User
+import string
+import random
 
 
-# Create your models here.
 class TgUser(models.Model):
-    tg_chat_id = models.BigIntegerField(verbose_name='TG CHAT_ID')
-    tg_user_id = models.BigIntegerField(unique=True, verbose_name='TG USER_ID')
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, default=None)
-    verification_code = models.CharField(max_length=10, unique=True)
-    username = models.CharField(
-        max_length=255,
-        verbose_name='TG USERNAME',
-        null=True,
-        blank=True,
-        default=None
-    )
+    tg_chat_id = models.BigIntegerField()
+    tg_user_id = models.BigIntegerField(unique=True)
+    user = models.ForeignKey('core.User', null=True, blank=True, verbose_name='Пользователь',
+                             on_delete=models.CASCADE)
+    verification_code = models.CharField(max_length=6, unique=True, null=True, blank=True)
 
     def set_verification_code(self):
-        length = 10  # Длина кода подтверждения
-        digits = string.digits
-        v_code = ''.join(random.sample(digits, length))
-        self.verification_code = v_code
+        code = string.digits + string.ascii_letters
+        verification_code = ''
+
+        for _ in range(6):
+            verification_code += code[random.randrange(0, len(code))]
+
+        self.verification_code = verification_code
+        self.save()
 
     class Meta:
-        verbose_name = 'TG User'
-        verbose_name_plural = 'TG Users'
+        verbose_name = 'TG пользователь'
+        verbose_name_plural = 'TG пользователи'
--- bot/tg/dc.py
+++ bot/tg/dc.py
@@ -1,30 +1,31 @@
-from dataclasses import field
-from typing import ClassVar, Type, List, Optional
+from dataclasses import dataclass, field
+from typing import List, Optional
 
-from marshmallow_dataclass import dataclass
-from marshmallow import Schema, EXCLUDE
+import marshmallow_dataclass
+from marshmallow import EXCLUDE
 
 
 @dataclass
 class MessageFrom:
     id: int
     is_bot: bool
-    first_name: str
+    first_name: Optional[str]
     last_name: Optional[str]
-    username: str
+    username: Optional[str]
 
     class Meta:
         unknown = EXCLUDE
 
 
 @dataclass
-class MessageChat:
+class Chat:
     id: int
-    first_name: Optional[str]
-    username: Optional[str]
-    last_name: Optional[str]
     type: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    username: Optional[str]
     title: Optional[str]
+    # photo: Optional[telegram.ChatPhoto]
 
     class Meta:
         unknown = EXCLUDE
@@ -34,18 +35,16 @@ class MessageChat:
 class Message:
     message_id: int
     from_: MessageFrom = field(metadata={'data_key': 'from'})
-    chat: MessageChat
+    chat: Chat
     date: int
-    text: str
-
-    # entities: list[MessageEntities]
+    text: Optional[str]
 
     class Meta:
         unknown = EXCLUDE
 
 
 @dataclass
-class UpdateOdj:
+class UpdateObj:
     update_id: int
     message: Message
 
@@ -56,9 +55,7 @@ class UpdateOdj:
 @dataclass
 class GetUpdatesResponse:
     ok: bool
-    result: List[UpdateOdj]
-
-    Schema: ClassVar[Type[Schema]] = Schema
+    result: List[UpdateObj]
 
     class Meta:
         unknown = EXCLUDE
@@ -69,81 +66,9 @@ class SendMessageResponse:
     ok: bool
     result: Message
 
-    Schema: ClassVar[Type[Schema]] = Schema
-
     class Meta:
         unknown = EXCLUDE
 
-# from dataclasses import dataclass, field
-# from typing import List, Optional
-#
-# import marshmallow_dataclass
-# from marshmallow import EXCLUDE
-#
-#
-# @dataclass
-# class MessageFrom:
-#     id: int
-#     is_bot: bool
-#     first_name: Optional[str]
-#     last_name: Optional[str]
-#     username: Optional[str]
-#
-#     class Meta:
-#         unknown = EXCLUDE
-#
-#
-# @dataclass
-# class Chat:
-#     id: int
-#     type: str
-#     first_name: Optional[str]
-#     last_name: Optional[str]
-#     username: Optional[str]
-#     title: Optional[str]
-#
-#     class Meta:
-#         unknown = EXCLUDE
-#
-#
-# @dataclass
-# class Message:
-#     message_id: int
-#     from_: MessageFrom = field(metadata={'data_key': 'from'})
-#     chat: Chat
-#     date: int
-#     text: Optional[str]
-#
-#     class Meta:
-#         unknown = EXCLUDE
-#
-#
-# @dataclass
-# class UpdateObj:
-#     update_id: int
-#     message: Message
-#
-#     class Meta:
-#         unknown = EXCLUDE
-#
-#
-# @dataclass
-# class GetUpdatesResponse:
-#     ok: bool
-#     result: List[UpdateObj]
-#
-#     class Meta:
-#         unknown = EXCLUDE
-#
-#
-# @dataclass
-# class SendMessageResponse:
-#     ok: bool
-#     result: Message
-#
-#     class Meta:
-#         unknown = EXCLUDE
-#
-#
-# GET_UPDATES_SCHEMA = marshmallow_dataclass.class_schema(GetUpdatesResponse)()
-# SEND_MESSAGE_SCHEMA = marshmallow_dataclass.class_schema(SendMessageResponse)()
+
+GET_UPDATES_SCHEMA = marshmallow_dataclass.class_schema(GetUpdatesResponse)()
+SEND_MESSAGE_SCHEMA = marshmallow_dataclass.class_schema(SendMessageResponse)()
