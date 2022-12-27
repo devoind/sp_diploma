a9976422799b21acd0aac4e19ef272b736581387
Deadpo_Ol <https://github.com/devoind>
2022-12-27 22:26:10 +0300

    New_BOT_4

--- bot/models.py
+++ bot/models.py
@@ -3,13 +3,21 @@ from django.db import models
 import string
 import random
 
+from core.models import User
+
 
 class TgUser(models.Model):
-    tg_chat_id = models.BigIntegerField()
-    tg_user_id = models.BigIntegerField(unique=True)
-    user = models.ForeignKey('core.User', null=True, blank=True, verbose_name='Пользователь',
-                             on_delete=models.CASCADE)
-    verification_code = models.CharField(max_length=6, unique=True, null=True, blank=True)
+    tg_chat_id = models.BigIntegerField(verbose_name='TG CHAT_ID')
+    tg_user_id = models.BigIntegerField(unique=True, verbose_name='TG USER_ID')
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, default=None)
+    verification_code = models.CharField(max_length=10, unique=True)
+    username = models.CharField(
+        max_length=255,
+        verbose_name='TG USERNAME',
+        null=True,
+        blank=True,
+        default=None
+    )
 
     def set_verification_code(self):
         code = string.digits + string.ascii_letters
