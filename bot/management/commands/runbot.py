76bb17d18864a5554a862cba9820e64a02d1289b
Deadpo_Ol <https://github.com/devoind>
2022-12-31 01:01:58 +0300

    Fix bot to create_goal

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -85,17 +85,17 @@ class Command(BaseCommand):
                 else:
                     self.tg_client.send_message(tg_user.tg_chat_id, 'Категория не существует')
 
-    def create_goal(self, tg_user: TgUser, category: GoalCategory):
-        """Создание новой цели через Telegram-Bot"""
-        self.tg_client.send_message(tg_user.tg_chat_id, 'Укажите название задачи. Для отмены введи /cancel')
+    def create_goal(self, tg_user, category):
+        self.tg_client.send_message(tg_user.tg_chat_id, 'Укажи название задачи. \n'
+                                                        'Для отмены введи /cancle')
 
-        flag = True
-        while flag:
-            response = self.tg_client.get_updates(offset=self.offset)
-            for item in response.result:
-                self.offset = item.update_id + 1
-                if item.message.text == '/cancel':
-                    flag = False
-                else:
-                    goal = Goal(title=item.message.text, category=category, user=tg_user.user)
-                    goal.save()
+        response = self.tg_client.get_updates(offset=self.offset)
+        for item in response.result:
+            self.offset = item.update_id + 1
+
+            if item.message.text == '/cancle':
+                continue
+            else:
+                goal = Goal(title=item.message.text, category=category, user=tg_user.user)
+                goal.save()
+                self.tg_client.send_message(tg_user.tg_chat_id, f'Создана задача {goal.title}')
